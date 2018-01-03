922bc8089df783c34d54624fe76f467c06d49fc4
msweene4 <msweene4@uwo.ca>
2018-01-03 13:42:26 -0500

    Team project with paulyhong2000

---
+++ server.js
@@ -0,0 +1,156 @@
+var mysql = require('mysql');
+
+var express = require('express'); // call express
+var app = express();                //defining our app using express
+var bodyParser = require('body-parser');
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+//setting the port to 8081
+var port =  8081;
+
+
+var con = mysql.createConnection({
+  host: "0.0.0.0",
+  user: "dhong45",
+  password: "",
+  database:"pets3309"
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
+
+
+var router = express.Router();
+
+// middleware to use for all requests
+router.use(function(req, res, next) 
+{
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    // do logging
+    console.log('Something is happening.');
+    next(); // make sure we go to the next routes and don't stop here
+});
+
+
+//testing route, GET at http://localhost:8080/api
+router.get('/', function(req, res) 
+{
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.json({ message: 'hooray! welcome to our api!' });   
+});
+
+router.get('/stores', function(req, res, next) {
+	 con.query('SELECT * from stores', function (error, results, fields) {
+		if (error) throw error;
+		res.send(JSON.stringify({"response": results}));
+	});
+});
+
+router.get('/soldPets', function(req,res,next) {
+    console.log("right before the sold pets sqlquery");
+	 con.query('SELECT pet.pName, transactionInst.TID, pet.PetID FROM transactionInst JOIN pet ON pet.PetID=transactionInst.PetID;', function (error, results, fields) {
+		if (error) throw error;
+		res.send(JSON.stringify({"response": results}));
+	});
+});
+
+router.post('/revenue', function(req,res,next)
+{
+  let storeID = req.body.storeID;
+  console.log("Howdy");
+  con.query(
+    {sql: 'select Sum(pet.price) AS pPrice, Sum(items.price) AS iPrice from Stransaction,transactionInst,pet,items where (Stransaction.TID=transactionInst.TID) AND (transactionInst.PetID=pet.PetID) AND (transactionInst.ItemID=items.ItemID) AND (Stransaction.storeID=?) order by transactionInst.InstID;'},
+    [storeID],
+    function(error, results, fields) {
+      if (error) throw error;
+      console.log("we made it in the revenue");
+		  res.send(JSON.stringify({"response": results}));
+    }
+  );
+});
+
+router.post('/change', function(req,res,next)
+{
+    let className = req.body.className;
+    let  price = req.body.price;
+    
+    con.query(
+      {sql: 'UPDATE pet SET price= ? where className=?'},
+      [price, className],
+      function(error, results, fields) {
+	      if (error) throw error;
+      }
+	 );
+	con.query(
+		    {sql: 'Select pName,PetID, price, className From pet where className =?'},
+		    [className],
+		    function(error, results, fields) {
+		        if (error) throw error;
+		        console.log("we made it in the query");
+		        res.send(JSON.stringify({"response": results}));
+		    }
+  );
+});
+
+router.post('/addpets', function(req, res, next) {
+   let className = req.body.className
+   let pName= req.body.pName;
+   let age = req.body.age;
+   let price = req.body.price;
+   let storeID = req.body.storeID;
+   let spayedNeutered = req.body.spayedNeutered;
+   //add customer with subquery to find
+   con.query(
+     {sql: 'INSERT INTO pet(className ,  pName, age, price, storeID, spayedNeutered) Values( ?, ?, ?, ?, ?, ?)'},
+     [className ,pName, age, price, storeID, spayedNeutered],
+     function(error, results, fields) {
+         if (error) throw error;
+         console.log("inserting new pet into pets database");
+     });
+     con.query(
+      {sql: 'Select * From pet'},
+      function(error, results, fields){
+        if (error) throw error;
+        console.log("we made it into the return statement");
+        res.send(JSON.stringify({"response": results}));
+    });
+});
+router.post('/remove', function(req,res,next)
+{
+  let EID = req.body.EID;
+  con.query(
+    {sql: 'DELETE FROM employee WHERE EID =?'},
+    [EID],
+    function(error, results, fields){
+      if (error) throw error;
+      console.log("deleting"+EID);
+    });
+  con.query(
+    {sql: 'Select * From employee'},
+    function(error, results, fields){
+      if (error) throw error;
+      console.log("we made it into the return statement");
+      res.send(JSON.stringify({"response": results}));
+    });
+});
+
+    
+    
+// REGISTER OUR ROUTES -------------------------------
+// all of our routes will be prefixed with /api
+app.use('/api', router);
+
+
+
+
+
+
+// START THE SERVER
+// =============================================================================
+app.listen(port);
+console.log('Magic happens on port ' + port);
---
+++ src/homepage.js
@@ -0,0 +1,197 @@
+/*global fetch*/
+/*global URLSearchParams*/
+
+function round(value, decimals) {
+  return Number(Math.round(value+'e'+decimals)+'e-'+decimals);
+}
+
+function removeChild() {
+    while ( document.getElementById('list').firstChild ) {
+      document.getElementById('list').removeChild( document.getElementById('list').firstChild );
+    }
+}
+
+const url = "https://finaldatabase-dhong45.c9users.io:8081/api";
+function firstbutton()
+{
+    removeChild();
+    var payload = {className: document.getElementById("textbox1a").value, price: parseInt(document.getElementById("textbox1b").value)};
+    var enco = new URLSearchParams(payload);
+    
+    window.fetch(url+'/change', {
+    	method: 'POST', 
+    	header: {'Accept': 'application/json', 'Content-Type': 'application/json'},
+    	body: enco,
+    })
+    .then((resp) => resp.json())
+    .then(function(data) {
+        console.log(data);
+        var i=0;
+        var line="";
+        
+        line=("pName    PetID    price    className");
+        var ul = document.getElementById("list");
+        var li = document.createElement("li");
+        li.appendChild(document.createTextNode(line));
+        ul.appendChild(li);
+        
+        while(i<data.response.length){
+            line=(data.response[i].pName+"  "+data.response[i].PetID+"  "+data.response[i].price+"  "+data.response[i].className);
+            var ul = document.getElementById("list");
+            var li = document.createElement("li");
+            li.appendChild(document.createTextNode(line));
+            ul.appendChild(li);
+            i=i+1;
+        }
+    })
+    .catch(function(error) {
+        console.log(JSON.stringify(error));
+    });
+}
+  
+function secondbutton()
+{
+    removeChild();
+    var payload = {EID: document.getElementById("textbox2").value};
+    var enco = new URLSearchParams(payload);
+    
+    window.fetch(url+'/remove', {
+    	method: 'POST',
+    	header: {'Accept': 'application/json', 'Content-Type': 'application/json'},
+    	body: enco,
+    })
+    .then((resp) => resp.json())
+    .then(function(data) {
+        console.log(data);
+        var i=0;
+        var line="";
+        
+        line=("EID    Ename");
+        var ul = document.getElementById("list");
+        var li = document.createElement("li");
+        li.appendChild(document.createTextNode(line));
+        ul.appendChild(li);
+        
+        while(i<data.response.length){
+            line=(data.response[i].EID+"  "+data.response[i].Ename);
+            var ul = document.getElementById("list");
+            var li = document.createElement("li");
+            li.appendChild(document.createTextNode(line));
+            ul.appendChild(li);
+            i=i+1;
+        }
+    })
+    .catch(function(error) {
+        console.log(JSON.stringify(error));
+    });
+}
+function thirdbutton()
+{
+    removeChild();
+    var payload = {storeID: document.getElementById("textbox3").value};
+    var enco = new URLSearchParams(payload);
+    
+    window.fetch(url+'/revenue', {
+    	method: 'POST', 
+    	header: {'Accept': 'application/json', 'Content-Type': 'application/json'},
+    	body: enco,
+    })
+    .then((resp) => resp.json())
+    .then(function(data) {
+        console.log(data);
+        
+        document.getElementById('answerbox3a').value=("$"+data.response[0].pPrice);
+        document.getElementById('answerbox3b').value=("$"+data.response[0].iPrice);
+    })
+    .catch(function(error) {
+        console.log(JSON.stringify(error));
+    });
+}
+
+
+
+function fourthbutton(){
+    removeChild();
+    console.log("fourthbutton");
+    fetch(url+'/soldPets')
+    .then((resp) => resp.json())
+    .then(function(data) {
+        console.log(data);
+        console.log(data.response[0].PetID);
+        var i=0;
+        while(i<data.response.length){
+
+             if(document.getElementById('textbox4').value==data.response[i].PetID){
+                 document.getElementById('answerbox4').value="The Pet you have selected has been sold";
+                 break;
+             }
+             else{
+                 document.getElementById('answerbox4').value="The pet has not been sold, or does not exist";
+             }
+             i=i+1;
+        }
+    })
+    .catch(function(error) {
+        console.log(JSON.stringify(error));
+    });
+}
+function fifthbutton()
+{
+    removeChild();
+    var payload = {className: document.getElementById("textbox5x").value, pName: document.getElementById("textbox5a").value, age: parseInt(document.getElementById("textbox5b").value), price: parseInt(document.getElementById("textbox5c").value), storeID: parseInt(document.getElementById("textbox5d").value), spayedNeutered: parseInt(document.getElementById("textbox5e").value)};
+    var enco = new URLSearchParams(payload);
+    
+    window.fetch(url+'/addpets', {
+    	method: 'POST', 
+    	header: {'Accept': 'application/json', 'Content-Type': 'application/json'},
+    	body: enco,
+    })
+    .then((resp) => resp.json())
+    .then(function(data) {
+        console.log(data);
+        var i=0;
+        var line="";
+        
+        line=("className    pName    age    price    storeID    spayedNeutered");
+        var ul = document.getElementById("list");
+        var li = document.createElement("li");
+        li.appendChild(document.createTextNode(line));
+        ul.appendChild(li);
+        
+        while(i<data.response.length){
+            line=(data.response[i].className+"  "+data.response[i].pName+"  "+data.response[i].age+"  "+data.response[i].price+"  "+data.response[i].storeID+"  "+data.response[i].spayedNeutered);
+            var ul = document.getElementById("list");
+            var li = document.createElement("li");
+            li.appendChild(document.createTextNode(line));
+            ul.appendChild(li);
+            i=i+1;
+        }
+    })
+    .catch(function(error) {
+        console.log(JSON.stringify(error));
+    });
+  
+}
+function exitbutton()
+{
+    removeChild();
+  window.close();
+}
+
+function isNumberKey(txt, evt) {
+
+    var charCode = (evt.which) ? evt.which : evt.keyCode;
+    if (charCode == 46) {
+        //Check if the text already contains the . character
+        if (txt.value.indexOf('.') === -1) {
+            return true;
+        } else {
+            return false;
+        }
+    } else {
+        if (charCode > 31
+             && (charCode < 48 || charCode > 57))
+            return false;
+    }
+    return true;
+}
